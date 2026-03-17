b1ed83db959a6518c3e39829c056372f8c4848c9
Joseph Kelly <jskelly84@gmail.com>
2026-03-16 22:40:19 -0700

    Iniitialize project.

---
+++ CLAUDE.md
@@ -0,0 +1,6 @@
+# ~/Developer/SwiftUIBaseball/CLAUDE.md
+
+import ~/.claude/CLAUDE_SWIFT_DEVELOPER.md
+
+- Use the swift package, [SwiftBaseball](https://github.com/josephskelly/SwiftBaseball.git), to fetch MLB stats.
+
---
+++ SwiftUIBaseballTests/SwiftUIBaseballTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIBaseballTests.swift
+//  SwiftUIBaseballTests
+//
+//  Created by Joseph Kelly on 3/16/26.
+//
+
+import Testing
+
+struct SwiftUIBaseballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
